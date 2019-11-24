74fc4dd8014bc35b35f8e678d8ac80d793cc0d24
michaelhagspiel <michael.hagspiel.93@gmail.com>
2019-11-24 21:12:37 +0100

    Added isFleetComplete and getMaxShipCount  with shipSize as parameter

--- src/module-info.java
+++ src/module-info.java
@@ -1,5 +1,6 @@
 module battleship {
     requires transitive javafx.controls;
     requires javafx.media;
+    requires java.desktop;
     exports sample;
 }
--- src/sample/Field.java
+++ src/sample/Field.java
@@ -93,10 +93,31 @@ public class Field
      shipCount für alle(!!) Schiffslängen die richtige Anzahl gezählt hat (z.B für length 2 ==4(Schiff)), dann true.*/
     public boolean isFleetComplete()
     {
-        return ((this.shipCount(2) == 4 && this.shipCount(3) == 3 && this.shipCount(4) == 2 && this.shipCount(5) == 1));//es gibt 4 2er ,3 3er  ,2 4er und 1 5er
+        return ((this.isFleetComplete(2) && this.isFleetComplete(3) && this.isFleetComplete(4) && this.isFleetComplete(5)));//es gibt 4 2er ,3 3er  ,2 4er und 1 5er
+    }
+    
+    public boolean isFleetComplete(int shipSize)
+    {
+        return this.shipCount(shipSize) == this.getMaxShipCount(shipSize);
     }
 
-    public boolean setShip(int x, int y, int length, Direction dire, int diffvectorx, int diffvectory)
+    private int getMaxShipCount(int shipSize) {
+    	 switch (shipSize)
+         {
+             case 2:
+                 return 4;
+             case 3:
+                 return 3;
+             case 4:
+            	 return 2;
+             case 5:
+                 return 1;
+             default:
+                 return -1;
+         }
+	}
+
+	public boolean setShip(int x, int y, int length, Direction dire, int diffvectorx, int diffvectory)
     {
 
     /*Zuerst überprüfen wir, ob die Anzahl der Schiffe, mit der Länge die wir gerade setzen wollen, nicht eh schon
