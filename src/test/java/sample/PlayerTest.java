75df37134b1fc3b2de59ac372df6cc32bacd06af
michaelhagspiel <michael.hagspiel.93@gmail.com>
2019-11-25 10:42:38 +0100

    Added PlayerTest again

---
+++ src/test/java/sample/PlayerTest.java
@@ -0,0 +1,18 @@
+package src.test.java.sample;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class PlayerTest {
+
+    @BeforeEach
+    void setUp() {
+
+    }
+
+    @Test
+    void name() {
+    }
+}
