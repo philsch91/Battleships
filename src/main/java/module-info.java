aeced17fa69c4d365bbf7caa1e22921d4de13225
philipp <philipp@Philipps-MacBook.local>
2020-01-15 18:30:04 +0100

    updated pom.xml

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,7 +1,9 @@
+/*
 module battleship {
     requires transitive javafx.controls;
     requires javafx.media;
     requires java.desktop;
     requires org.apache.logging.log4j;
+    requires java.sql;
     exports sample;
-}
+} */
